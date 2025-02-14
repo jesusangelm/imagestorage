8f6d2fcefd39b57889b5397d52d0fc497b956203
Jesus Marin <jesusangelm@gmail.com>
2025-02-14 12:15:53 -0400

    base index page for categories and items

--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @categories = categories()
+  end
+
   test "should get categories index" do
     get admin_categories_url
     assert_response :success
-    assert response.body.include?("Categories list")
+    assert response.body.include?("Categories List")
+    assert response.body.include?(@categories.first.name)
+    assert response.body.include?(@categories.first.desc)
   end
 end
--- test/controllers/admin/items_controller_test.rb
+++ test/controllers/admin/items_controller_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 class Admin::ItemsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @items = items()
+  end
+
   test "should get items index" do
     get admin_items_url
     assert_response :success
-    assert response.body.include?("Items list")
+    assert response.body.include?("Items List")
+    assert response.body.include?(@items.first.name)
+    assert response.body.include?(@items.first.desc)
+    assert response.body.include?(@items.first.category.name)
   end
 end
