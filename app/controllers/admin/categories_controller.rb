27bf0cdeccbd112ab31f4a17e7413cc976d4a97a
Jesus Marin <jesusangelm@gmail.com>
2025-02-17 14:54:41 -0400

    add create/update function for category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,42 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: [ :edit, :update ]
+
   def index
     @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to admin_categories_url
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.expect(category: [ :name, :desc ])
   end
 end
--- test/controllers/admin/categories_controller_test.rb
+++ test/controllers/admin/categories_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @categories = categories()
+    @existed_category = categories(:one)
   end
 
   test "should get categories index" do
@@ -11,5 +12,38 @@ class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert response.body.include?("Categories List")
     assert response.body.include?(@categories.first.name)
     assert response.body.include?(@categories.first.desc)
+  end
+
+  test "should show new category form" do
+    get new_admin_category_url
+    assert_response :success
+    assert response.body.include?("New Category Form")
+  end
+
+  test "should create a category" do
+    assert_difference("Category.count") do
+      post admin_categories_url, params: { category: { name: "categoria 1", desc: "desc categoria 1" } }
+    end
+
+    assert_redirected_to admin_categories_path
+  end
+
+  test "should show edit category form" do
+    get edit_admin_category_url(@existed_category)
+    assert_response :success
+    assert response.body.include?("Edit Category Form")
+    assert response.body.include?(@existed_category.name)
+    assert response.body.include?(@existed_category.desc)
+  end
+
+  test "should update a category" do
+    patch admin_category_url(@existed_category), params: {
+                                category: {
+                                  name: "categoria 1 actualizada",
+                                  desc: "modificada categoria 1"
+                                }
+    }
+
+    assert_redirected_to admin_categories_path
   end
 end
